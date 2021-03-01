477ae1716639d740a041f5ccb56612b9afaf70ed
yash chaudhari <yashchaudhari109@gmail.com>
2021-03-01 20:09:42 +0530

    ...

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect coloision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        # if segment == snake.head:
+        #     pass
+        if snake.head.distance(segment) < 10:
             game_is_on= False
             scoreboard.game_over()
 
